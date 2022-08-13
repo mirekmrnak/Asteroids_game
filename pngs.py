e06dfcb3c344e3c8de1f8f55fed88eb1ec9d360f
Miroslav Mrňák <miroslav.mrnak@gmail.com>
2022-08-13 18:25:32 +0200

    PNGS anchor setting optimized

--- pngs.py
+++ pngs.py
@@ -8,10 +8,6 @@ spaceships_pngs.append(spaceship2)
 spaceship3 = pyglet.image.load('images\spaceship3.png')
 spaceships_pngs.append(spaceship3)
 
-for png in spaceships_pngs:
-    png.anchor_x = png.width // 2
-    png.anchor_y = png.height // 2
-
 meteor_big_pngs = []
 meteor1 = pyglet.image.load('images\meteorGrey_big1.png')
 meteor_big_pngs.append(meteor1)
@@ -38,14 +34,13 @@ meteor_small_pngs.append(meteor9)
 meteor10 = pyglet.image.load('images\meteorGrey_tiny2.png')
 meteor_small_pngs.append(meteor10)
 
-#toto dát nakonec a najednou nastavit anchor pro všechny obrázky
-for meteor_size in (meteor_big_pngs, meteor_med_pngs, meteor_small_pngs):
-    for png in meteor_size:
+efects_pngs = []
+laser = pyglet.image.load('images\laser.png')
+efects_pngs.append(laser)
+
+for pictures in (spaceships_pngs, meteor_big_pngs, meteor_med_pngs, meteor_small_pngs, efects_pngs):
+    for png in pictures:
         png.anchor_x = png.width // 2
         png.anchor_y = png.height // 2
-
-laser = pyglet.image.load('images\laser.png')
-laser.anchor_x = laser.width // 2
-laser.anchor_y = laser.height // 2
 
 wallpaper = pyglet.image.load('images\wallpaper.png')
